afe6410bfc1f4659f6ea8d41a4a0ae18e2e02255
Monkey-Tea <lwh15113152493@gmail.com>
2024-04-12 20:19:08 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -106,5 +106,21 @@ public class EmployeeController {
 
         return Result.success(pageResult);
     }
+    //路径参数+@PathVariable
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号:{},{}",status,id);
+        //调用Service
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 
 }
