ea3e5cc356c4b6b8f1948da1a7d9eac84b4cab02
Monkey-Tea <lwh15113152493@gmail.com>
2024-04-16 21:39:45 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,7 +1,10 @@
 package com.sky.mapper;
 
+import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,4 +15,18 @@ public interface DishFlavorMapper {
      * @param flavors
      */
     void insertBatch(List<DishFlavor> flavors);
+
+
+    /***
+     * 根据菜品id删除对应的口味数据
+     * @param dishId
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品id集合批量删除关联的口味数据
+     * @param dishId
+     */
+    void deleteByIds(List<Long> dishId);
 }
