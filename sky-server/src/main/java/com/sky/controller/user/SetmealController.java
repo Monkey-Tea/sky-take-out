f54f7b2b4064733a22f3b7a468ae7e6bd802a628
Monkey-Tea <lwh15113152493@gmail.com>
2024-04-24 15:01:04 +0800

    导入商品浏览功能代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.user;
+
+
+import com.sky.constant.StatusConstant;
+import com.sky.entity.Setmeal;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import com.sky.vo.DishVarietyVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("userSetmealController")
+@RequestMapping("/user/setmeal")
+@Api(tags = "C端-套餐浏览接口")
+public class SetmealController {
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 条件查询
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询套餐")
+    public Result<List<Setmeal>> list(Long categoryId){
+        Setmeal setmeal = new Setmeal();
+        setmeal.setCategoryId(categoryId);
+        setmeal.setStatus(StatusConstant.ENABLE);
+
+        List<Setmeal> list = setmealService.list(setmeal);
+        return Result.success(list);
+    }
+
+    public Result<List<DishVarietyVO>> dishList(@PathVariable("id") Long id){
+       List<DishVarietyVO> list = setmealService.getDishVarietyById(id);
+       return Result.success(list);
+    }
+}
