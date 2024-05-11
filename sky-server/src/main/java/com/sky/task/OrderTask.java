2a10b401ea35d1236324165d9a156dd60f1cc8f6
Monkey-Tea <lwh15113152493@gmail.com>
2024-05-11 19:44:18 +0800

    订单状态定时处理代码开发

--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -46,7 +46,7 @@ public class OrderTask {
      * 处理一直处于派送中状态的订单
      */
     @Scheduled(cron = "0 0 1 * * ?")//每天凌晨1点触发一次
-    //@Scheduled(cron = "0/5 * * * * ?")//测试每五秒触发一次.
+    //@Scheduled(cron = "0/5 * * * * ?")//测试每五秒触发一次
     public void processDeliveryOrder(){
         log.info("定时处理处于派送中的订单:{}",LocalDateTime.now());
         LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
